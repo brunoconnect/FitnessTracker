64359685d37949464654d8777032e0d0ebce61b0
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-13 17:33:38 -0300

    regra de negocio verificaçoes

--- app/src/main/java/br/com/connect/appfitnesstracker/ImcActivity.kt
+++ app/src/main/java/br/com/connect/appfitnesstracker/ImcActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 
 class ImcActivity : AppCompatActivity() {
 
@@ -30,21 +31,37 @@ class ImcActivity : AppCompatActivity() {
 
             val result = calculateImc(weight, height)
             Log.d("teste", "result $result")
+
+            val imcResponseId = imcResponse(result)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun calculateImc(weight: Int, height: Int): Double{
+    @StringRes
+    private fun imcResponse(imc: Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+        }
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
 //        peso / (altura * altura)
-        return weight / ( (height / 100.0) * (height / 100.0) )
+        return weight / ((height / 100.0) * (height / 100.0))
     }
 
     private fun validate(): Boolean {
 //            nao pode inserir valores nulos / vazio
 //            nao pode inserir / comecar com 0
-        if (editWeight.text.toString().isNotEmpty()
-            && editHeight.text.toString().isNotEmpty()
-            && !editWeight.text.toString().startsWith("0")
-            && !editHeight.text.toString().startsWith("0")
+        if (editWeight.text.toString().isNotEmpty() && editHeight.text.toString()
+                .isNotEmpty() && !editWeight.text.toString()
+                .startsWith("0") && !editHeight.text.toString().startsWith("0")
         ) {
             return true
         }
