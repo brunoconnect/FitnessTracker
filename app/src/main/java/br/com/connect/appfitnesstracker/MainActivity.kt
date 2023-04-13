40c0fd48aebe8a55e8743812c3f9b3fe4a638414
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-13 15:26:08 -0300

    Activity e Intent

--- app/src/main/java/br/com/connect/appfitnesstracker/MainActivity.kt
+++ app/src/main/java/br/com/connect/appfitnesstracker/MainActivity.kt
@@ -1,11 +1,25 @@
 package br.com.connect.appfitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            Log.i("teste", "clicou!")
+
+            val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+        }
     }
 }
