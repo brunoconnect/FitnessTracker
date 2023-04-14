bdd19fea67fcefb4326c0862724d3d9020ca1a62
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-14 09:43:48 -0300

    gerenciando o teclado

--- app/src/main/java/br/com/connect/appfitnesstracker/ImcActivity.kt
+++ app/src/main/java/br/com/connect/appfitnesstracker/ImcActivity.kt
@@ -1,9 +1,11 @@
 package br.com.connect.appfitnesstracker
 
+import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -40,7 +42,7 @@ class ImcActivity : AppCompatActivity() {
 
                 .setTitle(getString(R.string.imc_response, result))
                 .setMessage(imcResponseId)
-                .setPositiveButton(    //opcao 2
+                .setPositiveButton(
                     android.R.string.ok
                 ) { dialog, which ->
 
@@ -48,12 +50,8 @@ class ImcActivity : AppCompatActivity() {
                 .create()
                 .show()
 
-//            opção 1 da mais trabalho pra escrever
-//            dialog.setPositiveButton(android.R.string.ok, object : DialogInterface.OnClickListener {
-//                override fun onClick(dialog: DialogInterface?, which: Int) {
-//
-//                }
-//            })
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager // Metodo que esconde o teclado logo apos abrir o popup
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
