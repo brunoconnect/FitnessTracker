a7703f8e4ef37a787e71ab084dea37612e2faf0f
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-17 15:59:50 -0300

    segundo caso de uso

--- app/src/main/java/br/com/connect/appfitnesstracker/TmbActivity.kt
+++ app/src/main/java/br/com/connect/appfitnesstracker/TmbActivity.kt
@@ -2,10 +2,21 @@ package br.com.connect.appfitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class TmbActivity : AppCompatActivity() {
+
+    private lateinit var lifestyle: AutoCompleteTextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tmb)
+
+        lifestyle = findViewById(R.id.auto_lifestyle)
+        val items = resources.getStringArray(R.array.tbm_lifestyle)
+        lifestyle.setText(items.first())
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        lifestyle.setAdapter(adapter)
+
     }
 }
