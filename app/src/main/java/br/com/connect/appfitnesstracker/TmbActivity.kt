e18fe431faa72bc7e9766d53d07529e010ebbf0b
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-17 15:35:04 -0300

    empilhamento de activity

---
+++ app/src/main/java/br/com/connect/appfitnesstracker/TmbActivity.kt
@@ -0,0 +1,11 @@
+package br.com.connect.appfitnesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TmbActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tmb)
+    }
+}
